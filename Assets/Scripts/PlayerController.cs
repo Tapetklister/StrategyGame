5e9fd586cf7e82bb776846822ca92db291ae3f7b
Anton (Lottas Dator) <antonnilssondeveloper@gmail.com>
2018-07-16 18:53:16 +0200

    Added player movement add collider. Also added collider to wallmap.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField] float m_MovementSpeed = 5.0f;
+
+    Vector2 m_Movement;
+
+    private void FixedUpdate()
+    {
+        transform.Translate(
+            m_Movement.x * m_MovementSpeed * Time.deltaTime,
+            m_Movement.y * m_MovementSpeed * Time.deltaTime,
+            0.0f
+        );
+    }
+
+    void Update()
+    {
+        m_Movement = new Vector2(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical")
+        );
+    }
+}
